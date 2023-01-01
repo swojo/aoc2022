a57393a1951622583a01c2e73b84db5d9706e984
swojo <samantha.wojtowicz@gmail.com>
2023-01-01 18:28:03 +0100

    Refactor day 2

--- src/advent/day2.clj
+++ src/advent/day2.clj
@@ -1,12 +1,15 @@
 (ns advent.day2
   (:require
+    [clojure.core :as c]
     [clojure.java.io :as io]
     [clojure.string :as str]))
+
 
 (def sample-input
   "A Y
 B X
 C Z")
+
 
 (defn parse-expected-outcome
   [in]
@@ -15,6 +18,7 @@ C Z")
     "Y" :tie
     "Z" :win))
 
+
 (defn parse-move
   [in]
   (case in
@@ -22,16 +26,20 @@ C Z")
     ("B" "Y") :paper
     ("C" "Z") :scissors))
 
+
 (defn eval-round
   [{:keys [:opponent :self] :as round}]
-  (let [outcome (if (= opponent self)
-                  :tie
-                  (case [opponent self]
-                    [:scissors :rock] :win
-                    [:paper :scissors] :win
-                    [:rock :paper] :win
-                    :lose))]
-    (assoc round :outcome outcome)))
+  (if (:outcome round)
+    round
+    (let [outcome (if (= opponent self)
+                    :tie
+                    (case [opponent self]
+                      [:scissors :rock] :win
+                      [:paper :scissors] :win
+                      [:rock :paper] :win
+                      :lose))]
+      (assoc round :outcome outcome))))
+
 
 (defn move-points
   [{:keys [:self]}]
@@ -40,6 +48,7 @@ C Z")
     :paper 2
     :scissors 3))
 
+
 (defn outcome-points
   [{:keys [:outcome]}]
   (case outcome
@@ -47,11 +56,13 @@ C Z")
     :tie 3
     :win 6))
 
+
 (defn calc-score
   [round]
   (assoc round :score
          (+ (move-points round)
             (outcome-points round))))
+
 
 (defn eval-round
   [{:keys [:opponent :self] :as round}]
@@ -64,20 +75,23 @@ C Z")
                     :lose))]
     (assoc round :outcome outcome)))
 
+
 (defn determine-move
   [{:keys [:opponent :outcome] :as round}]
+  (if (get round :self)
+    round
   (let [self (if (= outcome :tie)
-                  opponent
-                  (case [opponent outcome]
-                    [:scissors :lose] :paper
-                    [:scissors :win] :rock
-                    [:paper :lose] :rock
-                    [:paper :win] :scissors
-                    [:rock :lose] :scissors
-                    [:rock :win] :paper
-                    :lose))]
-    (assoc round :self self)
-  ))
+               opponent
+               (case [opponent outcome]
+                 [:scissors :lose] :paper
+                 [:scissors :win] :rock
+                 [:paper :lose] :rock
+                 [:paper :win] :scissors
+                 [:rock :lose] :scissors
+                 [:rock :win] :paper
+                 :lose))]
+    (assoc round :self self))))
+
 
 (defn parse-line-part1
   [line]
@@ -86,50 +100,41 @@ C Z")
      :self (parse-move self)}))
 
 
-(defn parse-input-part1
-  [input]
-  (->> (str/split-lines input)
-       (map parse-line-part1)))
-
 (defn parse-line-part2
   [line]
   (let [[opponent outcome] (str/split line #"\s+")]
     {:opponent (parse-move opponent)
      :outcome (parse-expected-outcome outcome)}))
 
-(defn parse-input-part2
-  [input]
+
+(defn parse-input
+  [input parse-line-fn]
   (->> (str/split-lines input)
-       (map parse-line-part2)))
+       (map parse-line-fn)))
 
-(defn solve-part1 [input] (->> (parse-input-part1 input)
-     (map eval-round)
-     (map calc-score)
-     (map :score)
-     (reduce + 0)))
 
-(defn solve-part2 [input]
-  (->> (parse-input-part2 input)
+(defn solve-part
+  [input parse-line-fn]
+  (->> (parse-input input parse-line-fn)
        (map determine-move)
+       (map eval-round)
        (map calc-score)
        (map :score)
-       (reduce + 0)
-       )
-  )
+       (reduce + 0)))
 
 (comment
    (parse-line-part1 (first (str/split-lines sample-input)))
    (determine-move (parse-line-part2 (last (str/split-lines sample-input))))
-   (solve-part2 sample-input)
-   (solve-part2 (slurp (io/resource "inputDay2.txt")))
-   (parse-input-part2 sample-input)
-  
+   (parse-input sample-input parse-line-part1) 
+   (parse-input sample-input parse-line-part2) 
 
    (parse-move "X")
    (eval-round {:opponent :rock,
                 :self :rock} )
-   (solve-part1 sample-input)
-   (solve-part1 (slurp (io/resource "inputDay2.txt")))
+   (solve-part sample-input parse-line-part1)
+   (solve-part sample-input parse-line-part2)
+   (solve-part (slurp (io/resource "inputDay2.txt")) parse-line-part1 )
+   (solve-part (slurp (io/resource "inputDay2.txt")) parse-line-part2)
  #__ )
 
 
