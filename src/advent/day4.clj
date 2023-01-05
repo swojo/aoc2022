4d8a6dc869efb9da6320f8414a44c34584c05165
swojo <samantha.wojtowicz@gmail.com>
2023-01-05 22:30:30 +0100

    Day 4 part 2

--- src/advent/day4.clj
+++ src/advent/day4.clj
@@ -1,8 +1,7 @@
 (ns advent.day4
   (:require
-    [clojure.string :as str]
     [clojure.java.io :as io]
-    ))
+    [clojure.string :as str]))
 
 
 (def sample-input
@@ -12,6 +11,7 @@
 2-8,3-7
 6-6,4-6
 2-6,4-8")
+
 
 (defn parse-input
   [input]
@@ -38,29 +38,60 @@
 
 
 (defn is-one-assignment-contained-by-other
-  [assignment1 assignment2]
+  [pair]
 
-  (cond
-    (and  (<= (first assignment1) (first assignment2))
-          (>= (last assignment1) (last assignment2)))
-    true
-    (and  (>= (first assignment1) (first assignment2))
-          (<= (last assignment1) (last assignment2)))
-    true
-    :else false))
+  (let [assignment1 (first pair)
+        assignment2 (last pair)
+        full-overlap
+        (cond
+          (and  (<= (first assignment1) (first assignment2))
+                (>= (last assignment1) (last assignment2)))
+          true
+          (and  (>= (first assignment1) (first assignment2))
+                (<= (last assignment1) (last assignment2)))
+          true
+          :else false)]
+    full-overlap))
 
-(defn solve
+
+(defn overlap-exists
+  [pair]
+  (let [assignment1 (first pair)
+        assignment2 (last pair)
+        partial-overlap
+        (cond
+          (and  (<= (first assignment1) (first assignment2))
+                (>= (last assignment1) (first assignment2)))
+          true
+          (and (>= (first assignment1) (first assignment2))
+               (<= (first assignment1) (last assignment2)))
+          true
+          :else false)]
+    partial-overlap))
+
+
+(defn solve-part1
   [input]
   (let [pairs (parse-input input)
-        contained (map (fn [pair] ( is-one-assignment-contained-by-other (first pair) (last pair))) pairs)
-        only-true (filter true? contained)
-        ]
-    
+        contained (map is-one-assignment-contained-by-other pairs)
+        only-true (filter true? contained)]
+
     (count only-true)))
 
-(comment 
-  (solve sample-input)
-  (solve (slurp (io/resource "inputDay4.txt")))
+(defn solve-part2
+  [input]
+  (let [pairs (parse-input input)
+        overlap (map overlap-exists pairs)
+        only-true (filter true? overlap)]
 
-  
+    (count only-true)))
+
+
+(comment 
+  (solve-part1 sample-input)
+  (solve-part1 (slurp (io/resource "inputDay4.txt")))
+
+  (solve-part2 sample-input)
+  (solve-part2 (slurp (io/resource "inputDay4.txt")))
+
   #__)
